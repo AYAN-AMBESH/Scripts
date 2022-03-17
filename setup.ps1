7474f75b88fff525562e41ea01919476e7cfb86f
AYAN-AMBESH <ambesh12k@gmail.com>
2022-03-17 23:53:20 +0530

    Automate Virtual Env

---
+++ setup.ps1
@@ -0,0 +1,30 @@
+if( Test-Path env:VIRTUAL_ENV)
+{
+ Write-Host 'You are already in a virtual enviorment'
+ $vnv= Read-Host 'Do you want to deactivate virtual env? y/n :'
+ if($vnv -eq 'y')
+ {
+    deactivate
+    Exit
+ }
+}
+$set_env = Read-Host 'Do you want to Download virtual env? y/n :'
+if($set_env -eq 'y')
+{
+    $env = Read-Host'Name your virtual enviorment: '
+    $down_env = virtualenv $env
+    & $down_env 
+}
+Write-Host 'Activating your virtual enviorment'
+$venv = Read-Host 'Enter your virtual enviorment name: '
+$ready = Read-Host 'Do you want to activate virtual enviorment? y/n :'
+
+if($ready -eq 'y')
+{
+    $activate_Env = "./$venv/bin/activate.ps1"
+    & $activate_Env
+}
+elseif ($ready -eq 'n') 
+{
+ Exit
+}
